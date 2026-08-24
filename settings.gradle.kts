8658c68c34484deb1194e81c4152d02acac3e1dd
tufazzal513 <mdtufazzal513@gmail.com>
2026-08-24 08:31:12 +0600

    build: downgrade chaquopy and update workflow
    Revert chaquopy to version 15.0.1 and remove the custom repository configuration. Simplify the CI workflow by invoking gradle directly to build the debug APK.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = java.net.URI("https://chaquo.com/maven") }
     }
 }
 dependencyResolutionManagement {
@@ -11,7 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = java.net.URI("https://chaquo.com/maven") }
     }
 }
 rootProject.name = "PyMobileIDE"
